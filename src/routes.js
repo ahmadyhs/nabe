360fc767087d22703496566f4d869312659495b2
ahmadyhs <ahmad20022@mail.unpad.ac.id>
2025-11-14 20:41:39 +0700

    init

---
+++ src/routes.js
@@ -0,0 +1,69 @@
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    }
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    }
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    }
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    }
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    }
+  },
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: (request, h) => {
+      return h.response('Alamat tidak ada').code(404);
+    },
+  },
+];
+
+module.exports = routes;
